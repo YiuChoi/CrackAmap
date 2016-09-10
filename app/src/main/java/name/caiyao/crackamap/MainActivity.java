33c6415db0642e936a5c1a78e8eacc000b991416
caiyao <whucaiyao@gmail.com>
2016-09-10 20:54:23 +0800

    Initial commit

---
+++ app/src/main/java/name/caiyao/crackamap/MainActivity.java
@@ -0,0 +1,105 @@
+package name.caiyao.crackamap;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity implements AMapLocationListener {
+    //声明mLocationOption对象
+    public AMapLocationClientOption mLocationOption = null;
+    AMapLocationClient mlocationClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+        mlocationClient = new AMapLocationClient(this);
+        //初始化定位参数
+        mLocationOption = new AMapLocationClientOption();
+        //设置定位监听
+        mlocationClient.setLocationListener(this);
+        //设置定位模式为高精度模式，Battery_Saving为低功耗模式，Device_Sensors是仅设备模式
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        //设置定位间隔,单位毫秒,默认为2000ms
+        mLocationOption.setInterval(2000);
+        //设置定位参数
+        mlocationClient.setLocationOption(mLocationOption);
+        // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+        // 注意设置合适的定位时间的间隔（最小间隔支持为2000ms），并且在合适时间调用stopLocation()方法来取消定位请求
+        // 在定位结束后，在合适的生命周期调用onDestroy()方法
+        // 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
+        //启动定位
+        mlocationClient.startLocation();
+    }
+
+    @Override
+    public void onLocationChanged(AMapLocation amapLocation) {
+        if (amapLocation != null) {
+            if (amapLocation.getErrorCode() == 0) {
+                //定位成功回调信息，设置相关消息
+                amapLocation.getLocationType();//获取当前定位结果来源，如网络定位结果，详见定位类型表
+                amapLocation.getLatitude();//获取纬度
+                amapLocation.getLongitude();//获取经度
+                amapLocation.getAccuracy();//获取精度信息
+                SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                Log.i("地址信息：",amapLocation.getAddress()+":"+amapLocation.getLongitude());
+                Date date = new Date(amapLocation.getTime());
+                df.format(date);//定位时间
+            } else {
+                //显示错误信息ErrCode是错误码，errInfo是错误信息，详见错误码表。
+                Log.e("AmapError", "location Error, ErrCode:"
+                        + amapLocation.getErrorCode() + ", errInfo:"
+                        + amapLocation.getErrorInfo());
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ mylibrary/src/main/java/com/amap/api/location/APSService.java
@@ -0,0 +1,84 @@
+package com.amap.api.location;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import com.amap.loc.ar;
+import com.amap.loc.b;
+import com.amap.loc.c;
+import com.amap.loc.cp;
+import com.amap.loc.q;
+
+public class APSService
+        extends Service {
+    APSServiceBase a;
+
+    public IBinder onBind(Intent paramIntent) {
+        try {
+            String str1 = paramIntent.getStringExtra("apiKey");
+            String str2 = paramIntent.getStringExtra("packageName");
+            String str3 = paramIntent.getStringExtra("sha1AndPackage");
+            String str4 = paramIntent.getStringExtra("as");
+            Log.i("修改前数据","apiKey:"+str1 + "\npackageName:" + str2 + "\nsha1AndPackage:" + str3 + "\nas:" + str4);
+            paramIntent.putExtra("apiKey","93076c13932aba6eeaee2a02656f14d7");
+            paramIntent.putExtra("packageName","com.example.example");
+            paramIntent.putExtra("sha1AndPackage","FA:AA:F5:02:B2:17:AC:D3:DA:46:38:ED:FF:E3:0C:A3:E4:67:0A:50:com.example.example");
+            str1 = paramIntent.getStringExtra("apiKey");
+            str2 = paramIntent.getStringExtra("packageName");
+            str3 = paramIntent.getStringExtra("sha1AndPackage");
+            str4 = paramIntent.getStringExtra("as");
+            Log.i("修改后数据","apiKey:"+str1 + "\npackageName:" + str2 + "\nsha1AndPackage:" + str3 + "\nas:" + str4);
+            return this.a.onBind(paramIntent);
+        } catch (Throwable localThrowable) {
+            c.a(localThrowable, "APSService", "onBind");
+        }
+        return null;
+    }
+
+    public void onCreate(Context paramContext) {
+        try {
+            boolean bool = cp.d(paramContext);
+            if (bool) {
+                q localq = c.a("loc", "2.9.0");
+                this.a = ((APSServiceBase) ar.a(paramContext, localq, "com.amap.api.location.APSServiceWrapper", b.class, new Class[]{Context.class}, new Object[]{paramContext}));
+            } else if (this.a == null) {
+                this.a = new b(this);
+            }
+        } catch (Throwable localThrowable1) {
+        }
+        try {
+            if (this.a == null) {
+                this.a = new b(this);
+            }
+            this.a.onCreate();
+        } catch (Throwable localThrowable2) {
+            c.a(localThrowable2, "APSService", "onCreate");
+        }
+        super.onCreate();
+    }
+
+    public void onCreate() {
+        onCreate(this);
+    }
+
+    public int onStartCommand(Intent paramIntent, int paramInt1, int paramInt2) {
+        try {
+            return this.a.onStartCommand(paramIntent, paramInt1, paramInt2);
+        } catch (Throwable localThrowable) {
+            c.a(localThrowable, "APSService", "onStartCommand");
+        }
+        return super.onStartCommand(paramIntent, paramInt1, paramInt2);
+    }
+
+    public void onDestroy() {
+        try {
+            this.a.onDestroy();
+        } catch (Throwable localThrowable) {
+            c.a(localThrowable, "APSService", "onDestroy");
+        }
+        super.onDestroy();
+    }
+}
